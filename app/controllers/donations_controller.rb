35c80fcc56a04c939e0e4660f9c0d2bf68b2509f
akkun08 <akihiroaika1112@gmail.com>
2021-02-08 11:36:08 +0900

    寄付の情報を新規保存する機能を実装

---
+++ app/controllers/donations_controller.rb
@@ -0,0 +1,28 @@
+class DonationsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+    user = User.create(user_params)
+    Address.create(address_params(user))
+    Donation.create(donation_params(user))
+    redirect_to action: :index
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :name_reading, :nickname)
+  end
+
+  def address_params(user)
+    params.permit(:postal_code, :prefecture, :city, :house_number, :building_name).merge(user_id: user.id)
+  end
+
+  def donation_params(user)
+    params.permit(:price).merge(user_id: user.id)
+  end
+end
